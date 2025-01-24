49593e6011dced3e4c5157a0f1473d2964ccecd5
GunterMagno <bcalejandro@iesrafaelalberti.es>
2025-01-24 21:27:46 +0100

    clase palabra y empezar jugador

---
+++ src/main/kotlin/Jugador.kt
@@ -0,0 +1,17 @@
+class Jugador() {
+    var intentos :Int = 0
+        private set
+
+    private val letrasUsadas = setOf<Char>()
+
+    fun intentarLetra(letra :Char) :Boolean{
+
+    }
+
+    fun fallarIntento(){intentos --}
+
+    fun obtenerLetrasUsadas() :String{
+
+    }
+
+}
